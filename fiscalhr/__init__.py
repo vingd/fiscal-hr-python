4a9de93df0fcd7a7dd58ac5f5f39e2e635766fa6
Andjelko Horvat <comel@vingd.com>
2013-12-16 16:56:32 +0100

    requirements.txt: remove distribute. Version 0.2.3.

--- fiscalhr/__init__.py
+++ fiscalhr/__init__.py
@@ -1,7 +1,7 @@
 '''Helper class for fiscalization in Croatia'''
 
 __title__ = 'FiscalHr'
-__version__ = '0.2.2'
+__version__ = '0.2.3'
 __author__ = 'Vingd, Inc.'
 __author_email__ = 'developers@vingd.com'
 __url__ = 'https://github.com/vingd/fiscal-hr-python'
