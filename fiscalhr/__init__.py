5cded6da1039a618a260467fb49f854a91dd01bd
Radomir Stevanovic <stevie@vingd.com>
2016-08-10 18:42:09 +0200

    bumped to v0.2.8

--- fiscalhr/__init__.py
+++ fiscalhr/__init__.py
@@ -1,7 +1,7 @@
 '''Helper class for fiscalization in Croatia'''
 
 __title__ = 'FiscalHr'
-__version__ = '0.2.7'
+__version__ = '0.2.8'
 __author__ = 'Vingd, Inc.'
 __author_email__ = 'developers@vingd.com'
 __url__ = 'https://github.com/vingd/fiscal-hr-python'
